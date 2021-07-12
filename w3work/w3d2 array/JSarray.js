1e551c3b782a75f10a20d41e3e9be06006311d23
Brett Ursprung <brett.ursprung@gmail.com>
2021-07-12 09:33:18 -0500

    add w3d3

--- w3work/w3d2 array/JSarray.js
+++ w3work/w3d2 array/JSarray.js
@@ -1,6 +1,4 @@
-
-
-let teachers = ['Shane', 'Zack'];
+let teachers = ["Shane", "Zack"];
 
 console.log(teachers);
 
@@ -9,45 +7,45 @@ console.log(teachers.push("Josh"));
 let arrayLength = teachers.length;
 let oldTeacher = teachers.pop();
 
-
 teachers.unshift("New Guy");
 let newGuy = teachers.shift();
 
 teachers[5] = "Mister Fiver";
 
-console.log("at index 5 " + teachers)
+console.log("at index 5 " + teachers);
 console.log("The array is now: " + arrayLength);
-console.log("the old teacher was: " +  oldTeacher);
+console.log("the old teacher was: " + oldTeacher);
 console.log(teachers);
 console.log("the new guy was: " + newGuy);
 
 console.log("Index of Shane: " + teachers.indexOf("Shane"));
 
 // slice shows the values between the first value and the second value
-let teacherSlice = teachers.slice(0,3);
+let teacherSlice = teachers.slice(0, 3);
 console.log("teacher slice is " + teacherSlice);
 console.log(teachers);
 
 // splice will replace the chosen values with a new value
-let teacherSplice = teachers.splice(0,3, "Josh");
+let teacherSplice = teachers.splice(0, 3, "Josh");
 console.log("teacher splice is: " + teacherSplice);
 console.log(teachers);
 
-// access each thing in the array,can log or see how many times 
+// access each thing in the array,can log or see how many times
 // something occurs in the array
 
-
 for (let i = 0; i < teachers.length; i++) {
-    console.log(teachers[i]);
+  console.log(teachers[i]);
 }
 
 let numberOfJoshs = 0;
-let indexOfJoshs = []
-for(let i = 0; i < teachers.length; i++); {
-    if (teachers[i] === "Josh"); {
-        numberOfJoshs++;
-        indexOfJoshs.push(i);
-    }
+let indexOfJoshs = [];
+for (let i = 0; i < teachers.length; i++);
+{
+  if (teachers[i] === "Josh");
+  {
+    numberOfJoshs++;
+    indexOfJoshs.push(i);
+  }
 }
 
 console.log(teachers);
---
+++ w3work/w3d3/w3d3.js
@@ -0,0 +1,18 @@
+console.log("script is loaded")
+
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+
+function add(a, b){
+     return a + b;
+ }
+
+console.log(add(2, 3));
+console.log(add(243, 3));
+console.log(add(2, 43));
+console.log(add(242, 342));
+console.log(add(24, 243));
+console.log(add(2, 443));
+console.log(add(2, 3242));
+console.log(add(242, 3));
